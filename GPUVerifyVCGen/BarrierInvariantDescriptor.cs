f5567e3826e0df17cbef0585b598472cb165a603
Nathan Chong <nathan.chong@gmail.com>
2013-02-20 02:48:32 +0000

    Clean-up path condition code for BIs

--- GPUVerifyVCGen/BarrierInvariantDescriptor.cs
+++ GPUVerifyVCGen/BarrierInvariantDescriptor.cs
@@ -28,13 +28,12 @@ namespace GPUVerify {
       if (CommandLineOptions.BarrierAccessChecks) {
         var visitor = new SubExprVisitor();
         visitor.VisitExpr(this.BarrierInvariant);
-        foreach (Tuple<Expr,Expr> pair in visitor.SubExprs) {
-          var Path = pair.Item1;
-          var e = (pair.Item2 as NAryExpr);
-          var v = (e.Args[0] as IdentifierExpr);
-          var index = e.Args[1];
+        foreach (Tuple<Expr,IdentifierExpr,Expr> pair in visitor.SubExprs) {
+          var cond = pair.Item1;
+          var v = pair.Item2;
+          var index = pair.Item3;
           this.AccessExprs.Add(Expr.Imp(Predicate, 
-            Expr.Imp(Path, BuildAccessedExpr(v.Name, index))));
+            Expr.Imp(cond, BuildAccessedExpr(v.Name, index))));
         }
       }
     }
@@ -67,11 +66,11 @@ namespace GPUVerify {
     }
 
     class SubExprVisitor : StandardVisitor {
-      internal HashSet<Tuple<Expr, Expr>> SubExprs;
+      internal HashSet<Tuple<Expr,IdentifierExpr,Expr>> SubExprs;
       internal List<Expr> Path;
 
       internal SubExprVisitor() {
-        this.SubExprs = new HashSet<Tuple<Expr,Expr>>();
+        this.SubExprs = new HashSet<Tuple<Expr,IdentifierExpr,Expr>>();
         this.Path = new List<Expr>();
       }
 
@@ -91,12 +90,12 @@ namespace GPUVerify {
         if (node.Fun is MapSelect) {
           Debug.Assert((node.Fun as MapSelect).Arity == 1);
           Debug.Assert(node.Args[0] is IdentifierExpr);
-          var v = (node.Args[0] as IdentifierExpr).Decl;
-          if (QKeyValue.FindBoolAttribute(v.Attributes, "group_shared") ||
-              QKeyValue.FindBoolAttribute(v.Attributes, "global")) {
-            var PathCondition = BuildPathCondition();
-            SubExprs.Add(new Tuple<Expr,Expr>(PathCondition,node));
-            Console.WriteLine("> Got one! {0} => {1}", PathCondition, node);
+          IdentifierExpr v = node.Args[0] as IdentifierExpr;
+          if (QKeyValue.FindBoolAttribute(v.Decl.Attributes, "group_shared") ||
+              QKeyValue.FindBoolAttribute(v.Decl.Attributes, "global")) {
+            Expr cond = BuildPathCondition();
+            Expr index = node.Args[1];
+            SubExprs.Add(new Tuple<Expr,IdentifierExpr,Expr>(cond,v,index));
           }
         } else if (node.Fun is BinaryOperator && 
                    (node.Fun as BinaryOperator).Op == BinaryOperator.Opcode.Imp) {
