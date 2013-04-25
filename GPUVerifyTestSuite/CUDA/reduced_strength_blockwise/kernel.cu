ab9698dbbacd2af18c0270a26b122b0c8922a211
Nathan Chong <nathan.chong@gmail.com>
2013-04-25 18:47:35 +0100

    Cope with block sizes which are prime numbers, eg --blockDim=3 -DWIDTH=3

--- GPUVerifyTestSuite/CUDA/reduced_strength_blockwise/kernel.cu
+++ GPUVerifyTestSuite/CUDA/reduced_strength_blockwise/kernel.cu
@@ -11,21 +11,29 @@
 
 __global__ void k(int *A) {
   __assert(blockDim.x <= WIDTH);
+#ifdef BLOCK_DIVIDES_WIDTH
+  __assert(__mod_pow2(WIDTH, blockDim.x) == 0);
+#endif
 
   for (int i=threadIdx.x; 
-    // working set using global invariants
+
+#ifndef BLOCK_DIVIDES_WIDTH
+    // working set(1) using global invariants
     /*A*/__global_invariant(__write_implies(A, (blockIdx.x*WIDTH) <= __write_offset(A)/sizeof(int))),
     /*B*/__global_invariant(__write_implies(A,                       __write_offset(A)/sizeof(int) < (blockIdx.x+1)*WIDTH)),
     /*C*/__invariant(threadIdx.x <= i),
     /*D*/__invariant(               i <= WIDTH+blockDim.x),
-
-  //// working set iff WIDTH % blockDim.x == 0
-  ///*A*/__invariant(__write_implies(A, (blockIdx.x*WIDTH) <= __write_offset(A)/sizeof(int))),
-  ///*B*/__invariant(__write_implies(A,                       __write_offset(A)/sizeof(int) < (blockIdx.x+1)*WIDTH)),
-  ///*C*/__invariant(threadIdx.x <= i),
-  ///*D*/__invariant(               i <= WIDTH+blockDim.x),
-  ///*E*/__invariant(__uniform_int((i-threadIdx.x))),
-  ///*F*/__invariant(__uniform_bool(__enabled())),
+         __invariant(i % blockDim.x == threadIdx.x),
+         __global_invariant(__write_implies(A, (((__write_offset(A)/sizeof(int)) % WIDTH) % blockDim.x) == threadIdx.x)),
+#else
+    // working set(2) iff WIDTH % blockDim.x == 0
+    /*A*/__invariant(__write_implies(A, (blockIdx.x*WIDTH) <= __write_offset(A)/sizeof(int))),
+    /*B*/__invariant(__write_implies(A,                       __write_offset(A)/sizeof(int) < (blockIdx.x+1)*WIDTH)),
+    /*C*/__invariant(threadIdx.x <= i),
+    /*D*/__invariant(               i <= WIDTH+blockDim.x),
+         __invariant(__uniform_int((i-threadIdx.x))),
+         __invariant(__uniform_bool(__enabled())),
+#endif
 
     i<WIDTH; i+=blockDim.x) {
     A[blockIdx.x*WIDTH+i] = i;
