becd4c56ca46dda200dc67d654faa0f13a4f6f91
Ally Donaldson <alastair.donaldson@imperial.ac.uk>
2013-11-14 20:31:12 +0000

    Adapt handling of atomic functions to take account of new map return parameter.

--- GPUVerifyVCGen/RaceInstrumenter.cs
+++ GPUVerifyVCGen/RaceInstrumenter.cs
@@ -488,7 +488,8 @@ namespace GPUVerify {
             AddLogAndCheckCalls(result,new AccessRecord((call.Ins[0] as IdentifierExpr).Decl,call.Ins[1]),AccessType.ATOMIC,null);
             (result[result.Count() - 1] as CallCmd).Attributes.AddLast((QKeyValue) call.Attributes.Clone()); // Magic numbers ahoy! -1 should be the check
             (result[result.Count() - 3] as CallCmd).Attributes.AddLast((QKeyValue) call.Attributes.Clone()); // And -3 should be the log
-            result.Add(new HavocCmd(Token.NoToken, new List<IdentifierExpr>(call.Outs.ToArray()))); // TODO: check this is right
+            Debug.Assert(call.Outs.Count() == 2); // The receiving variable and the array should be assigned to
+            result.Add(new HavocCmd(Token.NoToken, new List<IdentifierExpr> { call.Outs[0] })); // We havoc the receiving variable.  We do not need to havoc the array, because it *must* be the case that this array is modelled adversarially
             continue;
           }
         }
