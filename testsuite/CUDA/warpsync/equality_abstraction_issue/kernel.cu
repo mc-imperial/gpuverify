a26aec72e020328733e957a9cb54ba674fe0b916
Ally Donaldson <alastair.donaldson@imperial.ac.uk>
2013-11-19 20:48:45 +0000

    Slightly simplified test

--- testsuite/CUDA/warpsync/equality_abstraction_issue/kernel.cu
+++ testsuite/CUDA/warpsync/equality_abstraction_issue/kernel.cu
@@ -1,16 +1,11 @@
 //xfail:BOOGIE_ERROR
 //--warp-sync=32 --blockDim=32 --gridDim=1 --equality-abstraction
-//kernel.cu:14
+//kernel.cu:10
 
 __global__ void foo(int * A) {
-
     A[0] = 1;
     A[1] = 1;
     A[2] = 1;
-    A[3] = 1;
-    
     A[threadIdx.x] = 0;
-
-    __assert(A[0] == 1 | A[1] == 1 | A[2] == 1 | A[3] == 1);
-
+    __assert(A[0] == 1 | A[1] == 1 | A[2] == 1);
 }
