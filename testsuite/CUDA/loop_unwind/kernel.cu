c38cd6dc3a24268c2a7bfe341510e2bb334ce69d
Jeroen Ketema <j.ketema@imperial.ac.uk>
2013-11-30 01:08:29 +0000

    Fix text on Linux/Mac

--- testsuite/CUDA/loop_unwind/kernel.cu
+++ testsuite/CUDA/loop_unwind/kernel.cu
@@ -1,6 +1,6 @@
 //xfail:BOOGIE_ERROR
 //--blockDim=512 --gridDim=64 --loop-unwind=2
-//kernel.cu: error: possible write-write race on \(\(char\*\)[_a-zA-Z0-9]*B\)
+//kernel.cu: error: possible write-write race on \(\(char\*\)[_a-zA-Z0-9:]*B\)
 #include <cuda.h>
 
 extern "C" {
