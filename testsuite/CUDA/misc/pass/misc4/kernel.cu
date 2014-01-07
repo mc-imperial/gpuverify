3a3ba5a807e7a4b3f5fe4cc269b496eb97c7840c
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-01-07 22:57:20 +0000

    Add test case for noPruneInfeasibleEdges

---
+++ testsuite/CUDA/misc/pass/misc4/kernel.cu
@@ -0,0 +1,20 @@
+//pass
+//--gridDim=1024 --blockDim=1024
+
+__device__ void bar(int i);
+
+__global__ void
+foo(int w, int h)
+{
+   __requires(h == 0);
+
+   for (int i = threadIdx.x;
+        __invariant(h == 0),
+        i < w; i += blockDim.x)
+   {
+     if (h == 0)
+       bar(5);
+     else
+       __assert(0);
+   }
+}
