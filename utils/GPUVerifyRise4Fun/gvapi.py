f5e776fded7a567d4ec6277c11fabdc48d1f30f3
Dan Liew <daniel.liew@imperial.ac.uk>
2014-01-16 14:20:23 +0000

    Fix gvapi.py so it works with current version of GPUVerify. The issue occured because the error code names have been changed.

--- utils/GPUVerifyRise4Fun/gvapi.py
+++ utils/GPUVerifyRise4Fun/gvapi.py
@@ -27,7 +27,8 @@ ErrorCodes.OPT_ERROR:"Could not perform necessary optimisations to your kernel."
 ErrorCodes.BUGLE_ERROR:"Could not translate LLVM bitcode to Boogie.",
 ErrorCodes.GPUVERIFYVCGEN_ERROR:"Could not generate invariants and/or perform two-thread abstraction.",
 ErrorCodes.BOOGIE_ERROR:"",
-ErrorCodes.BOOGIE_TIMEOUT:"Verification timed out."
+ErrorCodes.TIMEOUT:"Verification timed out.",
+ErrorCodes.CONFIGURATION_ERROR:"The web service has been incorrectly configured. Please report this issue to gpuverify-support@googlegroups.com"
 }
 
 # Observer design pattern
