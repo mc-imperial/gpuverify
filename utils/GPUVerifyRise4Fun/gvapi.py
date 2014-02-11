301ba368c9b70e3c3a149ce111e8cbe6a76e07ec
Dan Liew <daniel.liew@imperial.ac.uk>
2014-02-11 20:39:10 +0000

    Fix an accidently committed merge conflict in gvapi.py

--- utils/GPUVerifyRise4Fun/gvapi.py
+++ utils/GPUVerifyRise4Fun/gvapi.py
@@ -128,11 +128,7 @@ class GPUVerifyTool(object):
                  '--only-divergence',
                  '--only-intra-group',
                  '--only-requires',
-<<<<<<< local
                  # '--parallel-inference', # Might bog down machine completely
-=======
-                 #'--parallel-inference', Might make DDOS easier, disable
->>>>>>> other
                  '--time',
                  '--staged-inference',
                  # r'--scheduling=[a-z-]+', # Not sure if safe
