1975a29f04ff53a50638939e573df5c385a50f78
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-02-11 20:29:52 +0000

    Disable some options; defensive checking of timeout

--- utils/GPUVerifyRise4Fun/gvapi.py
+++ utils/GPUVerifyRise4Fun/gvapi.py
@@ -66,7 +66,7 @@ class GPUVerifyTool(object):
       raise Exception('Could not find GPUVerify at "' + self.toolPath + '"')
 
     self.observers = [ ]
-    
+
   def registerObserver(self, observer):
     """
         Register an observer (of type GPUVerifyObserver) that will receive notifications
@@ -87,9 +87,9 @@ class GPUVerifyTool(object):
       source : The source code as a string where arguments will be extracted from.
       args   : A list that this function will populate
       ignoredArgs : If not None this list will be populated with arguments that
-                    were ignored (except the NDRange arguments like --blockDim).
+                    were ignored.
       additionalArgs : If not None this list be used as additional command line
-                       arguments. Just like the arguments in the source code these 
+                       arguments. Just like the arguments in the source code these
                        will be filtered.
     """
     if len(args) != 0:
@@ -110,7 +110,7 @@ class GPUVerifyTool(object):
                  '--array-equalities',
                  '--asymmetric-asserts',
                  r'--atomic=(r|rw|none)',
-                 '--debug',
+                 # '--debug', # developer option, should not be visible
                  #'--dynamic-analysis', # Note sure if safe, disable for now
                  '--equality-abstraction',
                  '--findbugs',
@@ -123,23 +123,22 @@ class GPUVerifyTool(object):
                  '--no-infer',
                  '--no-loop-predicate-invariants',
                  '--no-refinded-atomics',
-                 '--no-smart-predication',
-                 '--no-source-loc-infer',
+                 # '--no-smart-predication', # Most likely broken, disable
                  '--no-uniformity-analysis',
                  '--only-divergence',
                  '--only-intra-group',
                  '--only-requires',
-                 '--parallel-inference',
+                 # '--parallel-inference', # Might bog down machine completely
                  '--time',
                  '--staged-inference',
                  # r'--scheduling=[a-z-]+', # Not sure if safe
                  '--verify',
-                 '--verbose',
+                 # '--verbose', # developer option, should not be visible
                  r'--warp-sync=\d{1,3}',
                  # OpenCL NDRange arguments
-                 r'--global_size=(\d+|\[\d+(,\d+){0,2}\])', 
-                 r'--local_size=(\d+|\[\d+(,\d+){0,2}\])', 
-                 r'--num_groups=(\d+|\[\d+(,\d+){0,2}\])', 
+                 r'--global_size=(\d+|\[\d+(,\d+){0,2}\])',
+                 r'--local_size=(\d+|\[\d+(,\d+){0,2}\])',
+                 r'--num_groups=(\d+|\[\d+(,\d+){0,2}\])',
                  # CUDA grid arguments
                  r'--blockDim=(\d+|\[\d+(,\d+){0,2}\])',
                  r'--gridDim=(\d+|\[\d+(,\d+){0,2}\])'
@@ -158,7 +157,7 @@ class GPUVerifyTool(object):
         _logging.warning('Ignoring passed command line option "' + arg + '"')
         if ignoredArgs != None:
           ignoredArgs.append(arg)
-      
+
 
   def runOpenCL(self, source, args, timeout=10):
    return self.__runCommon( source, args, '.cl', timeout)
@@ -170,35 +169,39 @@ class GPUVerifyTool(object):
         an OpenCL kernel and a CUDA kernel.
 
         source : The program source code to be checked as a string
-        args : A list of command line arguments to pass to GPUVerify
+        cmdArgs : A list of command line arguments to pass to GPUVerify
         fileExtension : 'cl' or 'cu'
         timeout : An integer timeout (0 is no timeout)
     """
+
+    # Perform sanity check of timeout
+    if timeout <= 0:
+      raise Exception('timeout must be positive')
 
     cmdArgs.append("--timeout=" + str(timeout))
 
 
     # Create source file inside self.tempDir
-    f = tempfile.NamedTemporaryFile(prefix='gpuverify-source-', 
-                                    suffix=fileExtension, 
+    f = tempfile.NamedTemporaryFile(prefix='gpuverify-source-',
+                                    suffix=fileExtension,
                                     delete=False,
                                     dir=self.tempDir)
     response=None
     try:
       f.write(source.encode('utf8'))
       f.close()
-      
+
       # Add sourcefile name to cmdArgs
       cmdArgs.append(f.name)
 
       response = self.__runTool(cmdArgs)
       if response[0] == ErrorCodes.TIMEOUT:
-        _logging.error('GPUVerify timed out (ErrorCode:{})'.format(response[0])) 
+        _logging.error('GPUVerify timed out (ErrorCode:{})'.format(response[0]))
 
     finally:
       f.close()
       os.remove(f.name)
-    
+
     # Invoke any observers on the outcome of running the command
     for observer in self.observers:
       # Do not allow observers to cause their exceptions to cause the complete execution to fail.
@@ -236,18 +239,19 @@ class GPUVerifyTool(object):
 
     else:
       raise Exception('Could not get version')
-    
+
 
   def __runTool(self, cmdLineArgs):
     # Make temporary working directory inside self.tempDir
     tempDir = tempfile.mkdtemp(prefix='gpuverify-working-directory-temp',dir=self.tempDir)
-    
+
     returnCode = 0
     message=""
-    try: 
+    try:
       cmdArgs = [ sys.executable, self.toolPath ] + cmdLineArgs
       _logging.debug('Running :' + str(cmdArgs))
       process = subprocess.Popen( cmdArgs,
+                                    stdin = subprocess.PIPE,
                                     stdout = subprocess.PIPE,
                                     stderr = subprocess.STDOUT,
                                     cwd = tempDir,
@@ -256,11 +260,11 @@ class GPUVerifyTool(object):
       message, _NOT_USED = process.communicate() # Run tool
       returnCode = process.returncode
     except OSError as e:
-      returnCode=-1 
+      returnCode=-1
       message = 'Internal error. Could not run "' + self.toolPath + '"'
     finally:
       # Remove the tempDir
       shutil.rmtree(tempDir)
-    
+
     return ( returnCode, message )
 
