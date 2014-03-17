de0a8454f6d1ff75829ad0065b45073ee211809f
Dan Liew <daniel.liew@imperial.ac.uk>
2014-03-17 09:26:27 +0000

    GPUVerifyRise4Fun: KernelRecorderObserver on posix systems now changes permissions of logged kernel so that it is readable by the group. It is recommended that the directory for recorded kernels should has the setgid attribute set so that created kernels get the group owner of the directory.

--- utils/GPUVerifyRise4Fun/observers/kernelrecorder.py
+++ utils/GPUVerifyRise4Fun/observers/kernelrecorder.py
@@ -2,6 +2,7 @@
 import gvapi
 import logging
 import os
+import stat
 import tempfile
 
 _logging = logging.getLogger(__name__)
@@ -13,9 +14,12 @@ class KernelRecorderObserver(gvapi.GPUVerifyObserver):
 
   def receive(self, source, args, returnCode, output):
     _logging.info('Logging kernel')
-
+    
+    tFile = None
     with tempfile.NamedTemporaryFile(delete=False, dir=self.kernelDir, \
                                      suffix='.txt', prefix='kernel-') as f:
+      tFile = os.path.abspath(f.name)
+      _logging.debug('Logging to {}'.format(tFile))
       f.write("=== Command Line Arguments ===\n\n" + str(args)       + "\n\n")
       f.write("======= Kernel Source ========\n\n" + source          + "\n\n")
       f.write("======== Return Code =========\n\n" + str(returnCode) + "\n\n")
@@ -23,3 +27,8 @@ class KernelRecorderObserver(gvapi.GPUVerifyObserver):
       f.close()
 
     _logging.debug('Logging kernel done')
+
+    if os.name == 'posix':
+      # Tempfile makes files only readable by current user
+      # Change permissions so readable by group as well.
+      os.chmod(tFile, stat.S_IRUSR | stat.S_IRGRP)
