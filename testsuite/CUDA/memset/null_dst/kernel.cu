726349324e9d151ca95dad425dde9b5e69d23a51
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-05-28 11:01:32 +0100

    Add tests and update baseline

---
+++ testsuite/CUDA/memset/null_dst/kernel.cu
@@ -0,0 +1,10 @@
+//xfail:BOOGIE_ERROR
+//--gridDim=1 --blockDim=32 --no-inline
+
+#define memset(dst,val,len) __builtin_memset(dst,val,len)
+
+__device__ int bar(void);
+
+__global__ void kernel(uint4 *out) {
+  memset(0, 0, 16);
+}
