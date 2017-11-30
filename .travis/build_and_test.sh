79465f80d99cf75c73e601eab315ed48b8272777
Jeroen Ketema <j.ketema@xs4all.nl>
2017-11-30 19:46:12 +0100

    Check whether all required variables are set when running Travis scripts

--- .travis/build_and_test.sh
+++ .travis/build_and_test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -ev
 
+if [ -z ${DOWNLOADS_DIR+x} ]; then
+  echo "DOWNLOADS_DIR not set"
+  exit 1
+fi
+
+if [ -z ${LLVM_VERSION+x} ]; then
+  echo "LLVM_VERSION not set"
+  exit 1
+fi
+
 if [ "${GPUVERIFY_DIR}" == "${BUILD_ROOT}" ]; then
   ${GPUVERIFY_DIR}/.travis/build_gpuverify.sh
   ${GPUVERIFY_DIR}/.travis/build_bugle.sh
@@ -15,8 +25,16 @@ fi
 ${GPUVERIFY_DIR}/.travis/clone_build_and_install_libclc.sh
 
 if [ "${DEFAULT_SOLVER}" == "z3" ]; then
+  if [ -z ${Z3+x} ]; then
+    echo "Z3 not set"
+    exit 1
+  fi
   ${GPUVERIFY_DIR}/.travis/download_and_symlink_z3.sh
 elif [ "${DEFAULT_SOLVER}" == "cvc4" ]; then
+  if [ -z ${CVC4+x} ]; then
+    echo "CVC4 not set"
+    exit 1
+  fi
   ${GPUVERIFY_DIR}/.travis/download_and_symlink_cvc4.sh
 else
   echo "Unknown default solver: ${DEFAULT_SOLVER}"
