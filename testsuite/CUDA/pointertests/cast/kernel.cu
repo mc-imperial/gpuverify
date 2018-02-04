f55d9c43c845bac0b62b7b6f771f059a428d6226
Jeroen Ketema <j.ketema@xs4all.nl>
2018-02-04 13:18:58 +0100

    Change test that started passing due to additional invariants being speculated

--- testsuite/CUDA/pointertests/cast/kernel.cu
+++ testsuite/CUDA/pointertests/cast/kernel.cu
@@ -12,6 +12,6 @@ __global__ void foo(int* p) {
   __shared__ unsigned char x[N];
 
   for (unsigned int i=0; i<(N/4); i++) {
-    ((unsigned int *)x)[i] = 0;
+    ((unsigned int *)x)[i] = threadIdx.x;
   }
 }
