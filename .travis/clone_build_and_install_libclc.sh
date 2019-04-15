d5a90ecfc156327139364ece65913779b88b6aa1
Jeroen Ketema <j.ketema@xs4all.nl>
2019-04-15 21:21:07 +0200

    Fix the last known good version of libclc in Travis

--- .travis/clone_build_and_install_libclc.sh
+++ .travis/clone_build_and_install_libclc.sh
@@ -2,8 +2,9 @@
 set -ev
 
 cd ${BUILD_ROOT}
-git clone --depth=${CLONE_DEPTH} --branch=master https://llvm.org/git/libclc.git
+git clone --branch=master https://llvm.org/git/libclc.git
 cd libclc
+git checkout ${LIBCLC_VERSION}
 $PYTHON ./configure.py -g ninja --with-llvm-config=${LLVM_CONFIG} \
   --with-cxx-compiler=${CXX} --prefix=${BUILD_ROOT}/libclc-install \
   nvptx-- nvptx64--
