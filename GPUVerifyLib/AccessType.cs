1318c8a320054d3a88e3edf728e78a66b3058ba3
Jeroen Ketema <j.ketema@xs4all.nl>
2022-04-17 13:15:28 +0200

    Use GetAccessType to get AccessType instead of one-off solution

--- GPUVerifyLib/AccessType.cs
+++ GPUVerifyLib/AccessType.cs
@@ -28,20 +28,6 @@ namespace GPUVerify
             this.name = name;
         }
 
-        public static AccessType Create(string access)
-        {
-            if (access.ToUpper() == "READ")
-                return READ;
-
-            if (access.ToUpper() == "WRITE")
-                return WRITE;
-
-            if (access.ToUpper() == "ATOMIC")
-                return ATOMIC;
-
-            throw new NotSupportedException("Unknown access type: " + access);
-        }
-
         public override string ToString()
         {
             return name;
